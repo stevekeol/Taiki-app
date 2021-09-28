0a6af68309e24208e5b283488bd868e398c013d2
stevekeol <stevekeol.x@gmail.com>
2021-09-28 15:23:14 +0800

    :sparkles: 加入聊天频道和聊天页

--- src/screens/Chat.tsx
+++ src/screens/Chat.tsx
@@ -207,63 +207,192 @@
 //   );
 // }
 
-//-------------------------------------------------------------
+//-----------------------------RN聊天界面版--------------------------------
 
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+// import React, { useEffect, useState } from 'react';
+// import { StyleSheet, View } from 'react-native';
+// import { StreamChat } from 'stream-chat';
+// import { Channel, Chat, MessageInput, MessageList, OverlayProvider as ChatOverlayProvider } from 'stream-chat-react-native';
+// import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+
+// const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm9uIn0.eRVjxLvd4aqCEHY_JRa97g6k7WpHEhxL7Z4K4yTot1c';
+// const user = { id: 'ron' };
+// const chatClient = StreamChat.getInstance('q95x9hkbyd6p');
+
+// const connectUserPromise = chatClient.connectUser(user, userToken);
+// const channel = chatClient.channel('messaging', 'channel_id');
+
+// const ChannelScreen = () => {
+//   const { bottom } = useSafeAreaInsets();
+
+//   return (
+//     <ChatOverlayProvider bottomInset={bottom} topInset={0}>
+//       <SafeAreaView>
+//         <Chat client={chatClient}>
+//           {/* Setting keyboardVerticalOffset as 0, since we don't have any header yet */}
+//           <Channel channel={channel} keyboardVerticalOffset={0}>
+//             <View style={StyleSheet.absoluteFill}>
+//               <MessageList />
+//               <MessageInput />
+//             </View>
+//           </Channel>
+//         </Chat>
+//       </SafeAreaView>
+//     </ChatOverlayProvider>
+//   );
+// };
+
+
+// export default function App() {
+//   const [ready, setReady] = useState();
+
+//   useEffect(() => {
+//     const initChat = async () => {
+//       await connectUserPromise;
+//       await channel.watch();
+//       setReady(true);
+//     };
+
+//     initChat();
+//   }, []);
+
+//   if (!ready) {
+//     return null;
+//   }
+
+//   return (
+//     <SafeAreaProvider>
+//       <ChannelScreen channel={channel} />
+//     </SafeAreaProvider>
+//   );
+// }
+
+
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import { LogBox, SafeAreaView, StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator, useHeaderHeight } from '@react-navigation/stack';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { StreamChat } from 'stream-chat';
-import { Channel, Chat, MessageInput, MessageList, OverlayProvider as ChatOverlayProvider } from 'stream-chat-react-native';
-import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import {
+  Channel,
+  ChannelList,
+  Chat,
+  MessageInput,
+  MessageList,
+  OverlayProvider,
+  useAttachmentPickerContext,
+} from 'stream-chat-react-native';
 
-const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm9uIn0.eRVjxLvd4aqCEHY_JRa97g6k7WpHEhxL7Z4K4yTot1c';
-
-const user = { id: 'ron' };
+LogBox.ignoreAllLogs(true);
 
 const chatClient = StreamChat.getInstance('q95x9hkbyd6p');
-const connectUserPromise = chatClient.connectUser(user, userToken);
+const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm9uIn0.eRVjxLvd4aqCEHY_JRa97g6k7WpHEhxL7Z4K4yTot1c';
+const user = { id: 'ron' };
 
-const channel = chatClient.channel('messaging', 'channel_id');
+const filters = {
+  members: { $in: ['ron'] },
+  type: 'messaging',
+};
 
-const ChannelScreen = () => {
-  const { bottom } = useSafeAreaInsets();
+const sort = { last_message_at: -1 };
+
+const ChannelListScreen = ({ navigation }) => {
+  const { setChannel } = useContext(AppContext);
+
+  const memoizedFilters = useMemo(() => filters, []);
 
   return (
-    <ChatOverlayProvider bottomInset={bottom} topInset={0}>
-      <SafeAreaView>
-        <Chat client={chatClient}>
-          {/* Setting keyboardVerticalOffset as 0, since we don't have any header yet */}
-          <Channel channel={channel} keyboardVerticalOffset={0}>
-            <View style={StyleSheet.absoluteFill}>
-              <MessageList />
-              <MessageInput />
-            </View>
-          </Channel>
-        </Chat>
-      </SafeAreaView>
-    </ChatOverlayProvider>
+    <Chat client={chatClient}>
+      <View style={StyleSheet.absoluteFill}>
+        <ChannelList
+          filters={memoizedFilters}
+          onSelect={(channel) => {
+            setChannel(channel);
+            navigation.navigate('Channel');
+          }}
+          sort={sort}
+        />
+      </View>
+    </Chat>
   );
 };
 
-export default function App() {
-  const [ready, setReady] = useState();
+const ChannelScreen = ({ navigation }) => {
+  const { channel } = useContext(AppContext);
+  const headerHeight = useHeaderHeight();
+  const { setTopInset } = useAttachmentPickerContext();
 
   useEffect(() => {
-    const initChat = async () => {
-      await connectUserPromise;
-      await channel.watch();
-      setReady(true);
-    };
-
-    initChat();
-  }, []);
-
-  if (!ready) {
-    return null;
-  }
+    setTopInset(headerHeight);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [headerHeight]);
 
   return (
+    <SafeAreaView>
+      <Chat client={chatClient}>
+        <Channel channel={channel} keyboardVerticalOffset={headerHeight}>
+          <View style={StyleSheet.absoluteFill}>
+            <MessageList />
+            <MessageInput />
+          </View>
+        </Channel>
+      </Chat>
+    </SafeAreaView>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const AppContext = React.createContext();
+
+const App = () => {
+  const { bottom } = useSafeAreaInsets();
+
+  const [channel, setChannel] = useState();
+  const [clientReady, setClientReady] = useState(false);
+
+  useEffect(() => {
+    const setupClient = async () => {
+      await chatClient.connectUser(user, userToken);
+
+      setClientReady(true);
+    };
+
+    setupClient();
+  }, []);
+
+  return (
+      <AppContext.Provider value={{ channel, setChannel }}>
+        <OverlayProvider bottomInset={bottom}>
+          {clientReady && (
+            <Stack.Navigator
+              initialRouteName='ChannelList'
+              screenOptions={{
+                headerTitleStyle: { alignSelf: 'center', fontWeight: 'bold' },
+              }}
+            >
+              <Stack.Screen
+                component={ChannelScreen}
+                name='Channel'
+                options={() => ({
+                  headerBackTitle: 'Back',
+                  headerRight: () => <></>,
+                  headerTitle: channel?.data?.name,
+                })}
+              />
+              <Stack.Screen component={ChannelListScreen} name='ChannelList' options={{ headerTitle: 'Channel List' }} />
+            </Stack.Navigator>
+          )}
+        </OverlayProvider>
+      </AppContext.Provider>
+  );
+};
+
+export default () => {
+  return (
     <SafeAreaProvider>
-      <ChannelScreen channel={channel} />
+      <App />
     </SafeAreaProvider>
   );
-}
+};
